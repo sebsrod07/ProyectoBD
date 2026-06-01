c551f18bf194522c46c8ea2da42e6738bb7056be
sebsrod07 <macigan2012mail.com>
2026-06-01 12:46:12 -0600

    aa

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ var cadenaPrueba = builder.Configuration.GetConnectionString("DefaultConnection"
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<ProyectoBdContext>(options =>
-   options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-   ));
-builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddDbContext<ProyectoBdContext>(options =>
+//    options.UseSqlServer(
+//         builder.Configuration.GetConnectionString("DefaultConnection")
+//    ));
+// builder.Services.AddEndpointsApiExplorer();
 
-var app = builder.Build();
-app.MapControllers();
+ var app = builder.Build();
+// app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
 Dictionary<string, LoginInfo> sesiones = new();
