6fa4f85de46d47be341a1c9a8a2768d1aa647ef7
sebsrod07 <macigan2012mail.com>
2026-06-01 12:49:57 -0600

    Prueba2

--- Program.cs
+++ Program.cs
@@ -1,21 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Models;
 var builder = WebApplication.CreateBuilder(args);
-// --- INICIO DE LA PRUEBA DE FUEGO ---
-var cadenaPrueba = builder.Configuration.GetConnectionString("DefaultConnection");
-// --- FIN DE LA PRUEBA DE FUEGO ---
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddDbContext<ProyectoBdContext>(options =>
-//    options.UseSqlServer(
-//         builder.Configuration.GetConnectionString("DefaultConnection")
-//    ));
-// builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ProyectoBdContext>(options =>
+   options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+   ));
+builder.Services.AddEndpointsApiExplorer();
 
  var app = builder.Build();
-// app.MapControllers();
+app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
 Dictionary<string, LoginInfo> sesiones = new();
